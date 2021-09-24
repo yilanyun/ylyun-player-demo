5a9b405be95d2b0f90a3e38f21e159e369ea1444
liuwq <liuwq@yilan.tv>
2021-09-24 11:14:54 +0800

    【功能】修复itemview滑出界面播放器没有停止的问题

--- sdkdemo/src/main/java/com/yilan/sdk/sdkdemo/PlayerListActivity.java
+++ sdkdemo/src/main/java/com/yilan/sdk/sdkdemo/PlayerListActivity.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import com.yilan.sdk.common.ui.inter.OnItemClickListener;
 import com.yilan.sdk.common.ui.recycle.BaseViewHolder;
 import com.yilan.sdk.common.ui.recycle.IViewHolderCreator;
+import com.yilan.sdk.common.ui.recycle.ViewAttachedToWindowListener;
 import com.yilan.sdk.common.ui.recycle.YLRecycleAdapter;
 import com.yilan.sdk.player.ylplayer.PlayerState;
 import com.yilan.sdk.player.ylplayer.TaskInfo;
@@ -25,6 +26,8 @@ import com.yilan.sdk.sdkdemo.feed.FeedItemDecoration;
 import com.yilan.sdk.sdkdemo.feed.FeedMedia;
 import com.yilan.sdk.sdkdemo.feed.FeedViewHolder;
 
+import java.util.List;
+
 /**
  * 此页面演示，在列表页面播放时，如果做到跳转详情页无缝衔接
  */
@@ -34,6 +37,9 @@ public class PlayerListActivity extends AppCompatActivity {
     IYLPlayerEngine playerEngine;
     private int position;
     private ViewGroup container;
+    List<FeedMedia> mockFeed;
+    //当前播放的media
+    FeedMedia currentMedia;
 
     public static void start(Context context) {
         Intent intent = new Intent(context, PlayerListActivity.class);
@@ -73,9 +79,27 @@ public class PlayerListActivity extends AppCompatActivity {
                         }
                         PlayerListActivity.this.position = position;
                     }
+                }).viewAttachListener(new ViewAttachedToWindowListener() {
+                    @Override
+                    public void onViewAttachedToWindow(BaseViewHolder baseViewHolder) {
+
+                    }
+
+                    @Override
+                    public void onViewDetachedFromWindow(BaseViewHolder holder) {
+                        /**
+                         * itemview 从界面detach时候的回调，
+                         * 当前播放的视频与detach的itemview相对应的时候，停止当前视频的播放
+                         */
+                        if (recyclerView.getScrollState() != RecyclerView.SCROLL_STATE_IDLE
+                                && currentMedia == holder.getData()) {
+                            playerEngine.stop();
+                        }
+                    }
                 });
         recyclerView.setAdapter(adapter);
-        adapter.setDataList(MockData.getMockFeed());
+        mockFeed = MockData.getMockFeed();
+        adapter.setDataList(mockFeed);
     }
 
     /**
@@ -87,17 +111,18 @@ public class PlayerListActivity extends AppCompatActivity {
          */
         FeedViewHolder holder = (FeedViewHolder) recyclerView.findViewHolderForAdapterPosition(position);
         Bundle bundle = ActivityOptionsCompat.makeSceneTransitionAnimation(this, holder.contentContainer, "player").toBundle();
-        PlayerDetailActivity.start(this, MockData.getMockFeed().get(position).videoId, bundle);
+        PlayerDetailActivity.start(this, mockFeed.get(position).videoId, bundle);
     }
 
     private void playVideo(int position) {
         FeedViewHolder holder = (FeedViewHolder) recyclerView.findViewHolderForAdapterPosition(position);
         if (holder == null) return;
+        currentMedia = mockFeed.get(position);
         TaskInfo taskInfo = new TaskInfo.Builder()
-                .videoID(MockData.getMockFeed().get(position).videoId)
+                .videoID(currentMedia.videoId)
                 .coverID(R.id.layout_content)//此view 在播放器播放时会将其隐藏，通常为封面imageview
                 .title("测试视频")
-                .url(MockData.getMockFeed().get(position).url)
+                .url(currentMedia.url)
                 .build();
         playerEngine.play(taskInfo, holder.contentContainer);
     }
