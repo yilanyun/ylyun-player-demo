a5717f4f2f4814d8727f2625e2c1c2762fc2b6e6
liurongzhi <360465033@qq.com>
2021-11-16 16:08:30 +0800

    【功能】升级版本 3.4.1.2

--- sdkdemo/src/main/java/com/yilan/sdk/sdkdemo/MainActivity.java
+++ sdkdemo/src/main/java/com/yilan/sdk/sdkdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.yilan.sdk.sdkdemo;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -66,6 +67,9 @@ public class MainActivity extends AppCompatActivity {
                 NewPageActivity.start(MainActivity.this, NewPageActivity.FEED_FLOAT);
             }
         });
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{"android.permission.WRITE_EXTERNAL_STORAGE"},99);
+        }
     }
 
 }
