4e7f7be65a9131ef8f0f2081556a44350fc245df
liurongzhi <360465033@qq.com>
2022-01-06 14:30:32 +0800

    【功能】增加悬浮窗关闭停止播放

--- sdkdemo/src/main/java/com/yilan/sdk/sdkdemo/floatwindow/FloatManager.java
+++ sdkdemo/src/main/java/com/yilan/sdk/sdkdemo/floatwindow/FloatManager.java
@@ -25,6 +25,7 @@ public class FloatManager {
             @Override
             public void onClose() {
                 stopFloatWindow();
+                mVideoView.stop();
             }
         });
     }
